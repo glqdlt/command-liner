4535871f38b7ac5fdc22961f3f662c1339f224c1
glqdlt <dlfdnd0725@gmail.com>
2018-03-13 10:56:15 +0900

    add.js : toggle option add

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ program
     .version('0.1.0')
     .option('-u, --username <username>', 'Github Username(Maybe .. Email)')
     .option('-p, --password <password>', 'Github Password')
+    .option('-B, --baz','enable some baz')
     .option('-t, --type <type>','Type', /^(ssh|http)$/i)
     .parse(process.argv);
 
 if(program.username) console.log(` ${program.username} - username`);
 if(program.password) console.log(` ${program.password} - password`);
 if(program.type) console.log(` ${program.type} - type`);
+console.log(program.baz ? 'ok baz on':'baz off');
